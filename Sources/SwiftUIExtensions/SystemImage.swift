7f4a568d386c8ac05bd0e6bba1b33f5f51ec02be
Sam Deane <sam@elegantchaos.com>
2020-06-05 17:17:51 +0100

    Added a name type and some default constants (only for AppKit currently)

--- Sources/SwiftUIExtensions/SystemImage.swift
+++ Sources/SwiftUIExtensions/SystemImage.swift
@@ -8,17 +8,37 @@ import SwiftUI
 /// Abstraction for system images which can load them in a different way on the Mac.
 
 public struct SystemImage: View {
-    let name: String
+    public struct Name: ExpressibleByStringLiteral, RawRepresentable {
+        public typealias StringLiteralType = String
+        public let rawValue: String
+        public init(rawValue value: String) {
+            rawValue = value
+        }
+        public init(stringLiteral value: String) {
+            rawValue = value
+        }
+    }
+
+    let name: Name
     
-    public init(_ name: String) {
+    public init(_ name: Name) {
         self.name = name
     }
     
     public var body: some View {
         #if os(macOS)
-        return Image(name)
+        return Image(nsImage: NSImage(named: name.rawValue)!)
         #else
-        return Image(systemName: name)
+        return Image(systemName: name.rawValue)
         #endif
     }
 }
+
+#if os(macOS)
+
+public extension SystemImage.Name {
+    static var rowDelete: Self { return "NSStopProgressFreestandingTemplate" }
+    static var rowHandle: Self { return "NSListViewTemplate" }
+}
+
+#endif
